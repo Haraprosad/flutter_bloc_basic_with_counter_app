cc2f519dcf04cdede96be2c65b2838386a65895d
iamniloy <padma.haraprosad@gmail.com>
2022-11-13 13:00:04 +0600

    Use BlocConsumer

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -17,22 +17,20 @@ class MyHomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              BlocListener<CounterBloc, CounterState>(
-                listener: (context, state) {
-                  if (state is CounterIncrementState) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Successfully increased."),duration: Duration(seconds: 1),));
-                  }
-                  if (state is CounterDecrementState) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Successfully decreased."),duration: Duration(seconds: 1)));
-                  }
-                },
-                child: BlocBuilder<CounterBloc, CounterState>(
-                builder: (context, state) => Text(
-                  'Counter value: ${state.counterValue}',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+              BlocConsumer<CounterBloc, CounterState>(
+                   listener: (context, state) {
+                if (state is CounterIncrementState) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Successfully increased."),duration: Duration(milliseconds: 300),));
+                }
+                if (state is CounterDecrementState) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Successfully decreased."),duration: Duration(milliseconds: 300)));
+                }
+              },
+              builder: (context, state) => Text(
+                'Counter value: ${state.counterValue}',
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
               ),
     
